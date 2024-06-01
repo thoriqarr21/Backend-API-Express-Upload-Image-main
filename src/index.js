fcf4df7c8703c96d05f6668133479fad3ebca758
thoriqarr21 <amuhammadthoriq@gmail.com>
2024-06-01 15:59:00 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import cors from "cors";
 import ProductRoute from "../routes/ProductRoute.js";
 
 const app = express();
+const serverless = require('serverless-http');
+
 
 app.use(cors());
 app.use(express.json());
 app.use(FileUpload());
 app.use(express.static("public"));
-app.use(ProductRoute);
-
+app.use('.netlify/src/index',ProductRoute);
 app.listen(5030, ()=> console.log('Server Up and Running...'));
+module.exports.hadler = serverless(app)
